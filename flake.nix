83e0b8c65c7103f0b93352e4d379b463210b5102
Thomas Sanchez <me@thomas-sanchez.me>
2024-10-04 15:44:02 +0200

    chore: add flake

---
+++ flake.nix
@@ -0,0 +1,79 @@
+{
+  description = "Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+    }:
+    let
+      overlays = [
+        rust-overlay.overlays.default
+        (final: prev: {
+          rustToolchain =
+            let
+              rust = prev.rust-bin;
+            in
+            if builtins.pathExists ./rust-toolchain.toml then
+              rust.fromRustupToolchainFile ./rust-toolchain.toml
+            else if builtins.pathExists ./rust-toolchain then
+              rust.fromRustupToolchainFile ./rust-toolchain
+            else
+              rust.stable.latest.default.override { extensions = [ "rust-src" ]; };
+        })
+      ];
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs {
+              inherit overlays system;
+              config.allowUnfree = true;
+            };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              rustToolchain
+              pkg-config
+              gdb
+              lldb
+              jetbrains.rust-rover
+              cmake
+              cargo-audit
+              cargo-deny
+              cargo-edit
+              cargo-expand
+              cargo-machete
+              cargo-outdated
+              cargo-tarpaulin
+              cargo-watch
+              samply
+
+              #
+              nixfmt-rfc-style
+              aspell
+            ];
+          };
+        }
+      );
+    };
+}
